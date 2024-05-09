c7180b99956f552eb62c94d94d263bc3408ec5f6
DikiSunanda <diki3509@gmail.com>
2024-05-09 12:03:03 +0700

    Menambahkan folder pertemuan 2

---
+++ pertemuan2/latihan1.php
@@ -0,0 +1,28 @@
+<?php
+// Pengulangan
+// For
+for( $i = 0; $i <= 5; $i++) {
+    echo "Hello World! <br>";
+}
+
+
+// while
+$i = 1;
+while ( $i < 5 ){
+    echo "Hello World! <br>";
+    $i++;
+}
+
+//do.. while
+$i = 10;
+do {
+    echo "Hello World! <br>";
+    $i++;
+}while ($i < 10);
+
+// foreach : pengulangan khusus array
+
+
+
+
+?>
---
+++ pertemuan2/latihan3.php
@@ -0,0 +1,25 @@
+<?php
+//Pemgkondisian / Percabangan
+// IF ELSE
+    $x = 10;
+    if($x < 2){
+        echo "Well";
+    } else { 
+        echo "Salah";
+    }
+    echo '<br>';
+// IF , ELSE IF ELSE
+$y = 20;
+if ($y > 10){
+    echo "Ten";
+} else if ($y == 20){
+    echo "Ten - Ten";
+} else {
+    echo "Ten - Ten - Ten";
+}
+// TERNARY
+// SWITCH CASE
+
+
+
+?>
